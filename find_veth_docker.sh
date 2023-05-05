51e9b92bba67aeb0af62fb6dc835dc27ccb2a64f
cslev <cs.lev@gmx.com>
2023-05-05 16:38:28 +0800

    Refactor code to support all kind of containers without a need for a specific tool installed inside

--- find_veth_docker.sh
+++ find_veth_docker.sh
@@ -43,11 +43,11 @@ else
   cmd="sudo docker ps --format {{.Names}} -f name=$NAME"
  fi
 
- if [ -z $INTF ]
-  then
-    # c_print "Yellow" "No interface name specified in the container...Using default: ${INTF}!"
-    INTF="eth0"
-  fi
+if [ -z $INTF ]
+then
+  # c_print "Yellow" "No interface name specified in the container...Using default: ${INTF}!"
+  INTF="eth0"
+fi
 
 
 
@@ -55,7 +55,16 @@ else
 c_print "BBlue" "VETH@HOST\tCONTAINER"
 for i in $($cmd)
 do
-  veth_in_container=$(sudo docker exec $i ip a|grep ${INTF}@|cut -d ':' -f 1)
-  veth_in_host=$(sudo ip a|grep "if${veth_in_container}:"|cut -d ":" -f 2|cut -d '@' -f 1|sed "s/ //g")
-  echo -e "${veth_in_host}\t${i}"
+  #getting the PIDs of the containers
+  PID=$(docker inspect $i --format "{{.State.Pid}}")
+  #using the PID, we can get the interface index of the eth0 interfae inside the container
+  INDEX=$(cat /proc/$PID/net/igmp |grep $INTF| awk '{print $1}')
+  #using the index, we can identify the veth interface
+  veth=$(ip -br addr |grep "if${INDEX}"|awk '{print $1}'|cut -d '@' -f 1)
+
+  #residuals from previous version that required built-in tools inside the container, but keeping them for reference
+  #veth_in_container=$(sudo docker exec $i ip a|grep ${INTF}@|cut -d ':' -f 1)
+  #veth_in_host=$(sudo ip a|grep "if${veth_in_container}:"|cut -d ":" -f 2|cut -d '@' -f 1|sed "s/ //g")
+
+  echo -e "${veth}\t${i}"
 done
