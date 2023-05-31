3e50a42c0c6f9d4906d940aeab9cd3484a4ea00f
cslev <cs.lev@gmx.com>
2023-05-31 15:13:59 +0800

    add veth MAC address to output

--- find_veth_docker.sh
+++ find_veth_docker.sh
@@ -58,18 +58,18 @@ fi
 
 
 #getting the container names and interface data
-c_print "BBlue" "VETH@HOST\tIP\t\tMAC\t\t\tBridge@HOST\tCONTAINER"
+c_print "BBlue" "VETH@HOST\tVETH_MAC\t\tCONTAINER_IP\tCONTAINER_MAC\t\tBridge@HOST\t\tCONTAINER"
 for i in $($cmd)
 do
   #getting the PIDs of the containers
-  PID=$(docker inspect $i --format "{{.State.Pid}}")
+  PID=$(sudo docker inspect $i --format "{{.State.Pid}}")
   #using the PID, we can get the interface index of the eth0 interfae inside the container
-  INDEX=$(cat /proc/$PID/net/igmp |grep $INTF| awk '{print $1}')
+  INDEX=$(sudo cat /proc/$PID/net/igmp |grep $INTF| awk '{print $1}')
   #using the index, we can identify the veth interface
-  veth=$(ip -br addr |grep "if${INDEX}"|awk '{print $1}'|cut -d '@' -f 1)
-
+  veth=$(sudo ip -br addr |grep "if${INDEX}"|awk '{print $1}'|cut -d '@' -f 1)
+  veth_mac=$(sudo ip a|grep $veth -A 2|grep ether|awk '{print $2}')
   #check if there is any special subnet created instead of the default
-  network_mode=$(docker inspect $i|jq .[].HostConfig.NetworkMode | sed "s/\"//g")
+  network_mode=$(sudo docker inspect $i|jq .[].HostConfig.NetworkMode | sed "s/\"//g")
   if [ "$network_mode" == "default" ]
   then
     network="bridge"
@@ -77,18 +77,18 @@ do
     network=$network_mode
   fi
     
-  ip_address=$(docker inspect $i|jq .[].NetworkSettings.Networks.$network.IPAddress)
-  mac_address=$(docker inspect $i| jq .[].NetworkSettings.Networks.$network.MacAddress)
-  gateway=$(docker inspect $i| jq .[].NetworkSettings.Networks.$network.Gateway | sed "s/\"//g")
-  bridge=$(ip -br addr |grep $gateway|awk '{print $1}')
+  ip_address=$(sudo docker inspect $i|jq .[].NetworkSettings.Networks.$network.IPAddress | sed "s/\"//g")
+  mac_address=$(sudo docker inspect $i| jq .[].NetworkSettings.Networks.$network.MacAddress | sed "s/\"//g")
+  gateway=$(sudo docker inspect $i| jq .[].NetworkSettings.Networks.$network.Gateway | sed "s/\"//g")
+  bridge=$(sudo ip -br addr |grep $gateway|awk '{print $1}')
   #residuals from previous version that required built-in tools inside the container, but keeping them for reference
   #veth_in_container=$(sudo docker exec $i ip a|grep ${INTF}@|cut -d ':' -f 1)
   #veth_in_host=$(sudo ip a|grep "if${veth_in_container}:"|cut -d ":" -f 2|cut -d '@' -f 1|sed "s/ //g")
   if [ "$bridge" == "docker0" ]
   then
     #we need an extra TAB before Bridge
-    echo -e "${veth}\t${ip_address}\t${mac_address}\t${bridge}\t\t${i}"
+    echo -e "${veth}\t${veth_mac}\t${ip_address}\t${mac_address}\t${bridge}\t\t\t${i}"
   else
-    echo -e "${veth}\t${ip_address}\t${mac_address}\t${bridge}\t${i}"
+    echo -e "${veth}\t${veth_mac}\t${ip_address}\t${mac_address}\t${bridge}\t\t${i}"
   fi
 done
